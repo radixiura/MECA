23352270ab8ac5c56e905daa6a0fec14adcdd9e9
Radix_iura <radix_iura@protonmail.com>
2022-10-25 00:43:03 +0300

    Add files via upload

--- cmd/internal/reg/reg_funcs/get_login.go
+++ cmd/internal/reg/reg_funcs/get_login.go
@@ -1,1 +1,20 @@
-g
+package reg_funcs
+
+
+import (
+ "fmt"
+ "os"
+)
+
+
+var p = fmt.Print
+var pln = fmt.Println
+var fs = fmt.Fscan
+
+
+func Get_login() string {
+  var new_user_login string
+  p("Введите ваш новый логиH: ")
+  fs(os.Stdin, &new_user_login)
+  return new_user_login
+}
--- cmd/internal/reg/reg_funcs/get_password.go
+++ cmd/internal/reg/reg_funcs/get_password.go
@@ -1,1 +1,25 @@
-f
+package reg_funcs
+
+
+import (
+ "os"
+ "time"
+)
+
+
+func Get_password() string {
+  var new_user_password string
+  p("Введите ваш новый пароль. Он должен содержать более 8ми символов: ")
+  fs(os.Stdin, &new_user_password)
+	// Proverka dliny
+  l := len(new_user_password)
+  if l <= 8 {
+    time.Sleep(3 * time.Second)
+  	pln("Пароль должен содержать более 8ми символов. Попробуйте еще раз: ")
+  	Get_password()
+  } else if l >= 8 {
+  	pln("")
+    time.Sleep(3 * time.Second)
+  }
+  return new_user_password
+}
