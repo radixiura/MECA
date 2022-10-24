bf6cf022fe673115727dd6b66b4cecc22d08ff45
Radix_iura <radix_iura@protonmail.com>
2022-10-25 00:41:21 +0300

    Add files via upload

---
+++ cmd/internal/menu/menu_buttons/reading/history.go
@@ -0,0 +1,13 @@
+package reading
+
+
+import (
+	"fmt"
+)
+
+
+func Fmenu1_history() {
+	fmt.Println("--------------++")
+	fmt.Println("")
+	fmt.Println("Британия и США начали войну из‑за убийства свиньи.")
+}
---
+++ cmd/internal/menu/menu_buttons/reading/psychology.go
@@ -0,0 +1,13 @@
+package reading
+
+
+import (
+	"fmt"
+)
+
+
+func Fmenu1_psychology() {
+	fmt.Println("--------------++")
+	fmt.Println("")
+	fmt.Println("Нет для человека лучшего звука, чем его произнесенное имя.")
+}
