a33e6b9f52e82b0e92b812f368c21253241abc54
Radix_iura <radix_iura@protonmail.com>
2022-10-25 00:42:01 +0300

    Update login.go

--- cmd/internal/log/login.go
+++ cmd/internal/log/login.go
@@ -1,1 +1,17 @@
-g
+package log
+
+
+import(
+	"fmt"
+	"meca/cmd/internal/log/log_funcs"
+)
+
+
+func Login() string {
+	fmt.Println("LOGIN NOW")
+  var user_login = log_funcs.Get_login()
+  var user_password = log_funcs.Get_password()
+  var user string
+  user = user_login + user_password
+  return user
+}
