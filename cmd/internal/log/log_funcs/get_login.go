ac8a8cb45054cc0a39c8715bebe61edd00196aa9
Radix_iura <radix_iura@protonmail.com>
2022-10-25 00:42:22 +0300

    Add files via upload

--- cmd/internal/log/log_funcs/get_login.go
+++ cmd/internal/log/log_funcs/get_login.go
@@ -1,1 +1,14 @@
-g
+package log_funcs
+
+
+import (
+ "os"
+)
+
+
+func Get_login() string {
+  var user_login string
+  p("Введите ваш login: ")
+  fs(os.Stdin, &user_login)
+  return user_login
+}
--- cmd/internal/log/log_funcs/get_password.go
+++ cmd/internal/log/log_funcs/get_password.go
@@ -1,1 +1,18 @@
-g
+package log_funcs
+
+
+import (
+ "fmt"
+ "os"
+)
+
+
+var p = fmt.Print
+var fs = fmt.Fscan
+
+func Get_password() string {
+  var user_password string
+  p("Введите ваш пароль: ")
+  fs(os.Stdin, &user_password)
+  return user_password
+}
