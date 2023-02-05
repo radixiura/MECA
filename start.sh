dc8abfdb3585d6073b78648931fbfcb6d7b06a75
Radix_iura <radix_iura@protonmail.com>
2023-02-05 18:29:13 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+sudo apt install gcc-go
+sudo apt install golang-go
 
 sudo systemctl start postgresql
+
+Зайти в каталог проекта там же где этот файл
 
 go mod init meca
 
 go mod tidy
+go mod vendor
 
-cd cmd
-
-go run main.go
+cd cmd && go run main.go
