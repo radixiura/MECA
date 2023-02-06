89fdab5c73c5ea2331d20085119d5505efdb7297
Radix_iura <radix_iura@protonmail.com>
2023-02-06 05:38:43 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+// Linux //
 sudo apt install gcc-go
 sudo apt install golang-go
-
-sudo systemctl start postgresql
+sudo apt install postgres
 
 Зайти в каталог проекта там же где этот файл
 
@@ -11,4 +11,27 @@ go mod init meca
 go mod tidy
 go mod vendor
 
+(sql)
+
 cd cmd && go run main.go
+//* Linux *//
+
+// Sql //
+sudo systemctl start postgresql
+sudo service postgres start
+
+sudo passwd postgres -> "sir1"
+su -l postgres -> "sir1"
+psql
+
+CREATE USER mecahost WITH password 'fenr1rson';
+CREATE DATABASE mecadb;
+\c mecadb
+CREATE TABLE users( id SERIAL PRIMARY KEY, login CHARACTER VARYING(30), password CHARACTER VARYING(30), rank INTEGER );
+GRANT ALL PRIVILEGES ON DATABASE mecadb to mecahost;
+GRANT ALL ON users TO mecahost;
+
+\q
+exit
+clear
+//* Sql *//
